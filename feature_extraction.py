c58b4a33b2b570091408fd400988c3174c531792
Muhammed Hasan Celik <hasancelik@std.sehir.edu.tr>
2016-10-17 22:48:27 +0300

    init

---
+++ feature_extraction.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+def feature_set_1(point, grid):
+    """Computes the distance from the agent to the dark blue squares aligned
+       with it
+       Parameters
+       ----------
+       point: (x,y) the position of the agent in the grid
+       grid: 2d array representing a grid
+       Returns
+       -------
+       [f1,f2,f3,f4]: a list of features in the given format
+       """
+    f1,f2,f3,f4 = (0,0,0,0)
+
+    
+    return [f1,f2,f3,f4]
+
+def feature_set_2(point, grid):
+    """Computes the distance from the agent to the light blue squares
+       one unit away from it
+       Parameters
+       ----------
+       point: (x,y) the position of the agent in the grid
+       grid: 2d array representing a grid
+       Returns
+       -------
+       [f5,f6,f7,f8]: a list of features in the given format
+       """
+    f5,f6,f7,f8 = (0,0,0,0)
+
+    
+    return [f5,f6,f7,f8]
+
+def feature_set_3(point, grid):
+    """Computes the distance from the agent to the green square
+       Parameters
+       ----------
+       point: (x,y) the position of the agent in the grid
+       grid: 2d array representing a grid
+       Returns
+       -------
+       f9: type- float
+       """
+    f9=0
+    return f9
+    
+
+
---
+++ learning_phase.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+def eveluate_model(grid):
+    """For a given grid, return the policy for that grid
+       Parameters
+       ----------
+       grid: 2 dimensional array representing a grid
+       Return:
+       policy: type-dictionary, for states (1,1) up to (6,6)
+       """
+    policy = {}
+    return policy
